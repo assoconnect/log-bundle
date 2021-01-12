d3745c509461a90a07fcc4296080ae6b2a6d86c1
Florian Guimier <florian.guimier@assoconnect.com>
2021-01-12 23:24:35 +0100

    Handle generic ID type in the log entity

--- tests/Functional/Entity/FunctionalLog.php
+++ tests/Functional/Entity/FunctionalLog.php
@@ -5,8 +5,35 @@ declare(strict_types=1);
 namespace AssoConnect\LogBundle\Tests\Functional\Entity;
 
 use AssoConnect\LogBundle\Entity\Log;
+use Doctrine\ORM\Mapping as ORM;
+use Ramsey\Uuid\UuidInterface;
 
 class FunctionalLog extends Log
 {
+    public function __construct(
+        UuidInterface $id,
+        string $entityClass,
+        string $entityColumn,
+        ?string $entityOldValue,
+        string $entityId,
+        string $requestTrace,
+        \DateTime $createdAt = null
+    ) {
+        $this->entityId = $entityId;
 
+        parent::__construct($id, $entityClass, $entityColumn, $entityOldValue, $requestTrace, $createdAt);
+    }
+
+    /**
+     * @ORM\Column(type="uuid_binary_ordered_time")
+     */
+    protected string $entityId;
+
+    /**
+     * @return string
+     */
+    public function getEntityId(): string
+    {
+        return $this->entityId;
+    }
 }
