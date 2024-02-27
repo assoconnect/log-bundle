7acdb5413e442ec2673d1eab0333a7c39c7473d9
Romain Chicoisne <romain.chicoisne@assoconnect.com>
2024-02-27 11:16:01 +0100

    try catch on Money instanciation to please phpstan

--- tests/Serializer/LogSerializerTest.php
+++ tests/Serializer/LogSerializerTest.php
@@ -109,8 +109,12 @@ class LogSerializerTest extends KernelTestCase
         yield [new DateTimeZone('Europe/Paris'), '"Europe\/Paris"'];
         yield [[new DateTimeZone('Europe/Paris')], '["Europe\/Paris"]'];
 
-        yield [Money::EUR('100'), '"100 EUR"'];
-        yield [[Money::EUR('100')], '["100 EUR"]'];
+        try {
+            yield [Money::EUR('100'), '"100 EUR"'];
+            yield [[Money::EUR('100')], '["100 EUR"]'];
+        } catch (\InvalidArgumentException $e) {
+            yield ['error' => 'Invalid argument: ' . $e->getMessage()];
+        }
 
         yield [new Currency('EUR'), '"EUR"'];
         yield [[new Currency('EUR')], '["EUR"]'];
