1ff9149f7e4546937a4803f2f8c8c493fce1a3fe
LucasP <lucas.peuch@assoconnect.com>
2026-06-04 10:55:15 +0200

    fix: remove redundant assert on EntityManagerInterface

--- src/Subscriber/LoggerSubscriber.php
+++ src/Subscriber/LoggerSubscriber.php
@@ -9,7 +9,6 @@ use AssoConnect\LogBundle\Factory\LogFactoryInterface;
 use AssoConnect\LogBundle\Factory\RequestContextAwareLogFactoryInterface;
 use AssoConnect\LogBundle\Factory\SecurityContextAwareLogFactoryInterface;
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
-use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\Event\OnFlushEventArgs;
 use Doctrine\ORM\Events;
 
@@ -30,7 +29,6 @@ class LoggerSubscriber
     public function onFlush(OnFlushEventArgs $eventArgs): void
     {
         $em = $eventArgs->getObjectManager();
-        assert($em instanceof EntityManagerInterface);
         $unitOfWork = $em->getUnitOfWork();
         $cmf = $em->getMetadataFactory();
         $requestTrace = $this->getRequestTrace();
